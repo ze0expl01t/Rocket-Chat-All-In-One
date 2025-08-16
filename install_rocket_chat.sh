c93f1f846d744257b0de312508104c2cc686183c
ze0expl01t <arkanintermedia@gmail.com>
2025-08-16 11:25:31 +0700

    Update install_rocket_chat.sh

--- install_rocket_chat.sh
+++ install_rocket_chat.sh
@@ -3,8 +3,8 @@
 # Rocket.Chat Auto Install Script
 # For Ubuntu 20.04/22.04 LTS
 # Using Node.js v22, MongoDB 7.0.23, Rocket.Chat 7.9.3
-# Domain: trial.techchandra.com
-# Email: hostmaster@techchandra.com
+# Domain: trial.your.com
+# Email: hostmaster@your.com
 # Password: BABpbdVerOD7v5
 
 # Colors
@@ -15,8 +15,8 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # Variables
-DOMAIN="mars.techchandra.com"
-EMAIL="hostmaster@techchandra.com"
+DOMAIN="trial.your.com"
+EMAIL="hostmaster@your.com"
 PASSWORD="BABpbdVerOD7v5"
 ROCKETCHAT_VERSION="7.9.3"
 ROCKETCHAT_URL="https://releases.rocket.chat/${ROCKETCHAT_VERSION}/download"
